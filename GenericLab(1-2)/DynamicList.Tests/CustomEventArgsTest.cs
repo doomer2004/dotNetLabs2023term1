791547a384878b9429ed03a94e0b46b9bdaaab3a
Danyil Hizhytskyi <dgizhitskiy@gmail.com>
2023-10-25 15:12:57 +0300

    Unit tests

---
+++ GenericLab(1-2)/DynamicList.Tests/CustomEventArgsTest.cs
@@ -0,0 +1,43 @@
+﻿using DynamicList.CustomEventArgs;
+
+namespace DynamicList.Tests;
+
+public class CustomEventArgsTest : DynamicListTestBase
+{
+
+    [Fact]
+    public void ArrayAction_AddToArray_ReturnsTrue()
+    {
+        var newItem = 6;
+        TestArrayAction(ArrayAction.Add, list => _dynamicList.Insert(5 ,newItem),
+            eventArgs => Assert.Equal(eventArgs.Item, newItem));
+    }
+    
+    [Fact]
+    public void ArrayAction_RemoveFromArray_ReturnsTrue()
+    {
+        var item = 5;
+        TestArrayAction(ArrayAction.Remove, list => _dynamicList.Remove(item),
+            eventArgs => Assert.Equal(eventArgs.Item, item));
+    }
+    
+    [Fact]
+    public void ArrayAction_ClearArray_ReturnsTrue()
+    {
+        TestArrayAction(ArrayAction.Clear, list => _dynamicList.Clear(),
+            eventArgs => Assert.Empty(_dynamicList));
+    }
+    
+    private void TestArrayAction(ArrayAction action, Action<DynamicList<int>> arrayAction,
+        Action<NotifyArrayChangedEventArgs<int>> arrayChanged)
+    {
+        NotifyArrayChangedEventArgs<int>? eventArgs = null;
+        _dynamicList.ArrayChanged+= args => eventArgs = args;
+        
+        arrayAction(_dynamicList);
+        
+        Assert.NotNull(eventArgs);
+        Assert.Equal(eventArgs.Action, action);
+        arrayChanged(eventArgs!);
+    }
+}
---
+++ GenericLab(1-2)/DynamicList.Tests/DynamicListEnumeratorTest.cs
@@ -0,0 +1,33 @@
+﻿using DynamicList.CustomEventArgs;
+namespace DynamicList.Tests;
+
+
+public class DynamicListEnumeratorTest : DynamicListTestBase
+{
+    
+    [Fact]
+    public void GetEnumerator_GetLastElement_ReturnsFalse()
+    {
+        var collection = new DynamicList<int>(new int[] {1, 2});
+        var enumerator = collection.GetEnumerator();
+
+        enumerator.MoveNext();
+        enumerator.MoveNext();
+        
+        Assert.False(enumerator.MoveNext());
+    }
+
+    [Fact]
+    public void Enumerator_Reset_ReturnsTrue()
+    {
+        var enumerator = _dynamicList.GetEnumerator();
+        var expectedItem = 1;
+
+        enumerator.MoveNext();
+        enumerator.MoveNext();
+        enumerator.Reset();
+        
+        Assert.Equal(expectedItem, enumerator.Current);
+    }
+
+}
